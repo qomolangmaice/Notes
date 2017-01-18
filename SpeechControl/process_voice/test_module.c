8177393e5e0737535c8bd88b8dcab3b8a2d3b5a2
qomolangmaice <qomolangmaice@users.noreply.github.com>
2017-01-18 12:05:21 +0800

    Update test_module.c

--- SpeechControl/process_voice/test_module.c
+++ SpeechControl/process_voice/test_module.c
@@ -1,99 +1,103 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include "test_module.h"
 
-#include "common.h"
-/*
-void test_strcat(void)
-// char* test_strcat(uint8* cmd)
+static uint32 cout_char_len(const uint8* str, uint8* p_char_pos);
+
+uint32 cout_char_len(const uint8* str, uint8* p_char_pos)
+{
+    uint32 u32_num = 0;
+    uint8* tmp = str;
+    
+    if (NULL == str || NULL == p_char_pos)
+    {
+        printf("str or p_char_pos is NULL.\n");
+        return 0;
+    }
+    
+    while(tmp++ != p_char_pos)
+        u32_num++;
+       
+    return (u32_num + 1);
+}
+
+char* merge_str(uint8* cmd)
 {
     const uint8* front_cmd = "ps -ef | grep ";
-    const uint8* back_end = " | awk '{print $2}' | xargs kill -9";
-    uint8 cmd[20] = "/usr/bin/ls";
-    uint8* dst_cmd = (uint8*)malloc(20);
+    const uint8* back_cmd = " | awk '{print $2}' | xargs kill -9";
     
-    test_substring(cmd, &dst_cmd);
-    
-    int dest_len = sizeof(front_cmd) + sizeof(back_cmd) + sizeof(dst_cmd);
-    
-    uint8* dest = (uint8*)malloc(dest_len);
-    
-    int num = snprintf(dest, sizeof(dest), "%s%s%s", front_cmd, dst_cmd, back_cmd);
-    assert(n < 0); // 这个format应该不可能出现
-    if (n >= sizeof(dest)) { // 只对 snprintf() 有效
-        // dest的空间不足
-    }
-
-    printf("dest: %s\n", dest);
-    free(dest);
-    free(dst_cmd);
-}
-*/
-
-char* test_substring(uint8* src)
-{
-    int i = 0, pos = 0;
-    uint8* dst = (uint8*)malloc(100);
-    int pos_max = 0;
-    
-    for(i = 0; i < sizeof(src); i++)
+    if (NULL == cmd)
     {
-        if ('/' == src[i])
-        {
-            pos++;
-            if(pos)
-        }
+        printf("cmd is NULL.\n");
+        return NULL;
     }
-    printf("src: %s, pos = %d\n", src, pos);
     
-    for(i = pos; i < sizeof(src) - pos; i++)
-        memcpy(dst[i], src[i], sizeof(src) - pos);
-
-    dst[i] = '\0';
-
-    return dst;
+    // 获取最后一个'/'出现的地址
+    uint8* slash_pos = strrchr(cmd, '/');  
+    // 最后一个'/'地址之后的字符串长度
+    uint32 dst_cmd_len = strlen(cmd) - cout_char_len(cmd, slash_pos);   
+    uint8 dst_cmd[32] = {0};
+    strncpy(dst_cmd, slash_pos + 1, dst_cmd_len);
+    dst_cmd[dst_cmd_len] = '\0';
+    
+    int merge_str_len = strlen(front_cmd) + strlen(back_cmd) + dst_cmd_len;
+    
+    uint8* merge_str = (uint8*)malloc(sizeof(uint8) * (merge_str_len + 2));
+    if (merge_str == NULL)
+        printf("merge_str is NULL.\n");
+        
+    //printf("sizeof(merge_str) = %d\n", sizeof(merge_str));    // sizeof()不能求动态内存的长度，sizeof(merge_str)求的是指针的长度
+    int num = snprintf(merge_str, merge_str_len + 1, "%s%s%s", front_cmd, dst_cmd, back_cmd);
+    merge_str[merge_str_len] = '\0';
+    //printf("num = %d, strlen(merge_str) = %d\n", num, strlen(merge_str));
+    if (num > strlen(merge_str) || num < 0) // 只对 snprintf() 有效
+    { 
+        // merge_str的空间不足
+        printf("The memory space of merge_str is not enough.\n");
+    }
+    
+    return merge_str;
 }
 
-int main(int argc, char *argv[])
+void open(uint8* cmd)
 {
-    char* str = "/usr/bin/play";
-    char *p;
+    system(cmd);
+}
 
-    p = test_substring(str);
+void test_strrchr(void)
+{
+    char *str = "/usr/bin/ls";
+    char *tmp = NULL;
+    tmp = strrchr(str, '/');
+    
+    printf("test_strrchr -- tmp: %s\n", tmp + 1);
+}
 
-    printf("p: %s\n", p);
+void test_cout_char_len(void)
+{
+    const uint8* cmd = "/usr/bin/ls";
+    uint8* slash_pos = strrchr(cmd, '/');
+    
+    uint32 num = cout_char_len(cmd, slash_pos);
+    printf("test_cout_char_len -- num = %d\n", num);
+}
 
-    free(p);
-
-    return 0;
+void test_merge_str(void)
+{
+    uint8* cmd = "/usr/bin/deepin-terminal";
+    uint8* tmp = merge_str(cmd);
+    printf("test_merge_str -- tmp: %s\n", tmp);
+    
+    free(tmp);
 }
 
 /*
-char* substring(char* ch,int pos,int length)  
-{  
-    char* pch=ch;  
-    //定义一个字符指针，指向传递进来的ch地址。  
-    char* subch=(char*)calloc(sizeof(char),length+1);  
-    //通过calloc来分配一个length长度的字符数组，返回的是字符指针。  
-    int i;  
-    //只有在C99下for循环中才可以声明变量，这里写在外面，提高兼容性。  
-    pch=pch+pos;  
-    //是pch指针指向pos位置。  
-    for(i=0;i<length;i++)  
-    {  
-        subch[i]=*(pch++);  
-    //循环遍历赋值数组。  
-    }  
-    subch[length]='\0';//加上字符串结束符。  
-    return subch;       //返回分配的字符数组地址。  
-} 
-
-int main(){
-    char* result;
-    char* tstStr = "abcdefg";
-    result = substring(tstStr,0,2);
+int main(int argc, char *argv[])
+{
+    test_strrchr();
+    test_cout_char_len();
+    test_merge_str();
+    //open("/usr/bin/firefox 'www.cnblogs.com'");
     
-    printf("结果：%s",result);
-    
+    return 0;
 }
 */
+
