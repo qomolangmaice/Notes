670e789aec26c322bc988c22e083d52d5433fb80
qomolangmaice <qomolangmaice@users.noreply.github.com>
2017-01-18 12:08:04 +0800

    Create test_module.h

---
+++ SpeechControl/process_voice/test_module.h
@@ -0,0 +1,13 @@
+#ifndef __TEST_MODULE_H__
+#define __TEST_MODULE_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef unsigned char uint8;
+typedef unsigned int uint32;
+
+extern char* merge_str(uint8* cmd);
+
+#endif
