e5181547fda978208a9ae5000b3bc498926dd0b2
qomolangmaice <qomolangmaice@users.noreply.github.com>
2017-01-18 12:09:59 +0800

    Update process_voice.c

--- SpeechControl/process_voice/process_voice.c
+++ SpeechControl/process_voice/process_voice.c
@@ -1,21 +1,20 @@
 #include "process_voice.h"
+#include "test_module.h"
 
 typedef struct PROCESS_VOICE
 {
-   uint8* utterance;                    // 语音识别后获得的语句
-   uint8* cmd;                          // 系统命令
-   void (*p_process_func)(uint8* cmd);  // 处理系统命令的函数
+   uint8* utterance;
+   uint8* cmd;
+   void (*p_process_func)(uint8* cmd);
 }PROCESS_VOICE_T;
 
-//static void test_strcat(void);
-//static void test_substring(uint8* src, uint8** dest);
 static void process_open(uint8* cmd);
-//static void process_close(uint8* cmd);
+static void process_close(uint8* cmd);
 
 const PROCESS_VOICE_T s_process_voice[] = 
 {
-  {"打开 终端", "/usr/share/deepin-terminal/main", process_open}
-  //{"关闭 终端", "/usr/share/deepin-terminal/main", process_close}
+  {"打开 终端", "/usr/bin/gnome-terminal", process_open},
+  {"关闭 终端", "/usr/bin/gnome-terminal", process_close}
 };
 
 void process_open(uint8* cmd)
@@ -23,15 +22,17 @@ void process_open(uint8* cmd)
     system(cmd);
 }
 
-/*******************************************************************************
-* Function Name  	: process_voice
-* Description    	: 处理语音识别后获得的字符串所包含的内容
-* Input          	: hyp -- 处理语音识别后获得的字符串
-* Output         	: None
-* Return         	: None
-*******************************************************************************/
+void process_close(uint8* cmd)
+{
+    uint8* tmp = merge_str(cmd);
+    system(tmp);
+    
+    free(tmp);
+}
+
 void process_voice(uint8* hyp)
 {
+    int i = 0;
     //pthread_t *t;
     
     //pthread_create(p_process_func ...);
@@ -39,7 +40,7 @@ void process_voice(uint8* hyp)
     
     //play_warning_tone();
     
-    for(int i = 0; i < ITEM_OF(s_process_voice); i++)
+    for(i = 0; i < ITEM_OF(s_process_voice); i++)
     {
         if(strcmp(hyp, s_process_voice[i].utterance) == 0)
         {
@@ -52,8 +53,7 @@ void process_voice(uint8* hyp)
 int main(void)
 {
     process_voice("打开 终端");
+    process_voice("关闭 终端");
     
     return 0;
 }
-
-
