acf80a6e93f90e054dc2cef7b63126294b411515
qomolangmaice <qomolangmaice@users.noreply.github.com>
2017-01-19 15:51:02 +0800

    Update test_module.c

--- SpeechControl/process_voice/test_module.c
+++ SpeechControl/process_voice/test_module.c
@@ -60,9 +60,42 @@ char* merge_str(char* cmd)
     return merge_str;
 }
 
+int execute_cmd(const char * cmdstring)
+{
+    pid_t pid;
+    int status;
+
+    if(cmdstring == NULL)
+    {
+         return (1);
+    }
+    
+    if((pid = fork()) < 0)
+    {
+        status = -1;
+    }
+    else if(pid == 0)
+    {
+        execl("/bin/sh", "sh", "-c", cmdstring, (char *)0);
+        _exit(127); //子进程正常执行则不会执行此语句
+    }
+    else
+    {
+    /*
+        while(waitpid(pid, &status, 0) < 0){
+            if(errno != EINTER){
+                status = -1;
+                break;
+            }
+        }
+        */
+    }
+    return status;
+}
+
 void open(char* cmd)
 {
-    system(cmd);
+    execute_cmd(cmd);
 }
 
 void test_strrchr(void)
