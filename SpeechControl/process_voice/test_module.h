67e4ddf98c72c5cd61875904a10a1acb362fb8ae
qomolangmaice <qomolangmaice@users.noreply.github.com>
2017-01-19 15:54:22 +0800

    Update test_module.h

--- SpeechControl/process_voice/test_module.h
+++ SpeechControl/process_voice/test_module.h
@@ -30,4 +30,26 @@ typedef unsigned int uint32;
  */
 extern char* merge_str(char* cmd);
 
+/************************* 对外提供函数声明 *****************/
+/**
+ * @ingroup		  语音识别处理模块
+ * @brief       执行命令函数
+ *
+ * @details 
+ * @par 描述：  【应用场景】pocketsphinx处理语音识别
+ *            
+ * @attention 
+ *             【注意事项】 执行命令函数
+ *             【依赖关系说明】无
+ *             【时序说明】无
+ *             【兼容性】无
+ *             【扩展性】无
+ * @param [IN]  cmd -- 待执行得命令
+ * @param [OUT] 无
+ * @retval		  无				
+ * @author  
+ * @date 2017-01-18 12:12
+ */
+extern int execute_cmd(const char * cmdstring);
+
 #endif
